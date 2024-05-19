f39db45ead8d4a93d5d550d72d6fcff331471f3e
Pavel <60060559+pyrogn@users.noreply.github.com>
2024-05-19 12:08:25 +0300

    Make better db tests, close properly

--- src/dual_choice/db.py
+++ src/dual_choice/db.py
@@ -7,79 +7,80 @@ DATABASE_URL = os.getenv(
 )
 assert DATABASE_URL
 
-# Initialize the connection pool
-async_pool: AsyncConnectionPool
 
+class Database:
+    def __init__(self, database_url: str):
+        self.database_url = database_url
+        self.pool: AsyncConnectionPool | None = None
 
-async def init_async_pool():
-    global async_pool
-    async_pool = AsyncConnectionPool(conninfo=DATABASE_URL, open=False)
-    await async_pool.open()
-    return async_pool
+    async def init_pool(self):
+        self.pool = AsyncConnectionPool(conninfo=self.database_url, open=False)
+        await self.pool.open()
+        return self.pool
 
+    async def execute_sql(self, query: str, params: list[Any] | None = None):
+        if params is None:
+            params = []
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(query, params)
+                await conn.commit()
 
-async def init_db():
-    await execute_sql("""
-        DROP TABLE IF EXISTS image_selections
-    """)
-    await execute_sql("""
-        CREATE TABLE IF NOT EXISTS image_selections (
-            user_id TEXT,
-            image_id INTEGER,
-            selected_id INTEGER,
-            other_id INTEGER,
-            timestamp timestamp default current_timestamp,
-            PRIMARY KEY (user_id, image_id, selected_id, other_id)
-        )
-    """)
-
-
-async def execute_sql(query: str, params: list[Any] | None = None):
-    if params is None:
-        params = []
-    async with async_pool.connection() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute(query, params)
-            await conn.commit()
-
-
-async def insert_selection(
-    user_id: str, image_id: int, selected_id: int, other_id: int
-):
-    await execute_sql(
-        """
-        INSERT INTO image_selections (user_id, image_id, selected_id, other_id)
-        VALUES (%s, %s, %s, %s)
-    """,
-        [user_id, image_id, selected_id, other_id],
-    )
-
-
-async def get_prop_selected(
-    image_id: int, selected_sub_id: int, non_selected_sub_id: int
-):
-    async with async_pool.connection() as conn:
-        async with conn.cursor() as cur:
-            await cur.execute(
-                """
-                SELECT
-                    COUNT(CASE WHEN selected_id = %s AND other_id = %s THEN 1 ELSE NULL END) AS selected_count,
-                    COUNT(1) AS total_count
-                FROM image_selections
-                WHERE image_id = %s AND (
-                    (selected_id = %s AND other_id = %s) OR
-                    (selected_id = %s AND other_id = %s)
-                )
-            """,
-                [
-                    selected_sub_id,
-                    non_selected_sub_id,
-                    image_id,
-                    selected_sub_id,
-                    non_selected_sub_id,
-                    non_selected_sub_id,
-                    selected_sub_id,
-                ],
+    async def init_db(self, clean=False):
+        if clean:
+            await self.execute_sql("""
+                DROP TABLE IF EXISTS image_selections
+            """)
+        await self.execute_sql("""
+            CREATE TABLE IF NOT EXISTS image_selections (
+                user_id TEXT,
+                image_id INTEGER,
+                selected_id INTEGER,
+                other_id INTEGER,
+                timestamp timestamp default current_timestamp,
+                PRIMARY KEY (user_id, image_id, selected_id, other_id)
             )
-            result = await cur.fetchone()
-            return result
+        """)
+
+    async def insert_selection(
+        self, user_id: str, image_id: int, selected_id: int, other_id: int
+    ):
+        await self.execute_sql(
+            """
+            INSERT INTO image_selections (user_id, image_id, selected_id, other_id)
+            VALUES (%s, %s, %s, %s)
+            """,
+            [user_id, image_id, selected_id, other_id],
+        )
+
+    async def get_prop_selected(
+        self, image_id: int, selected_sub_id: int, non_selected_sub_id: int
+    ):
+        async with self.pool.connection() as conn:
+            async with conn.cursor() as cur:
+                await cur.execute(
+                    """
+                    SELECT
+                        COUNT(CASE WHEN selected_id = %s AND other_id = %s THEN 1 ELSE NULL END) AS selected_count,
+                        COUNT(1) AS total_count
+                    FROM image_selections
+                    WHERE image_id = %s AND (
+                        (selected_id = %s AND other_id = %s) OR
+                        (selected_id = %s AND other_id = %s)
+                    )
+                    """,
+                    [
+                        selected_sub_id,
+                        non_selected_sub_id,
+                        image_id,
+                        selected_sub_id,
+                        non_selected_sub_id,
+                        non_selected_sub_id,
+                        selected_sub_id,
+                    ],
+                )
+                result = await cur.fetchone()
+                return result
+
+
+db = Database(DATABASE_URL)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,68 +1,84 @@
+import asyncio
 import psycopg
 import pytest
 import pytest_asyncio
-from dual_choice.db import get_prop_selected, init_db, insert_selection, init_async_pool
-
-
-# solve the problem with closed loop (cannot create second pytest function)
+from dual_choice.db import db
 
 
 @pytest_asyncio.fixture(scope="function")
 async def setup_database():
-    pool = await init_async_pool()
-    await init_db()
-    yield pool
-    # clean db here maybe
+    await db.init_pool()
+    await db.init_db(clean=True)
+    yield db.pool
+    await db.pool.close()
 
 
 @pytest.mark.asyncio
 async def test_insert_selection(setup_database):
     pool = setup_database
-    await insert_selection("user1", 1, 2, 3)
-    await insert_selection("user2", 1, 2, 3)
-    await insert_selection("user3", 1, 3, 2)
+    await db.insert_selection("user1", 1, 2, 3)
+    await db.insert_selection("user2", 1, 2, 3)
+    await db.insert_selection("user3", 1, 3, 2)
 
     # noise
-    await insert_selection("user1", 1, 2, 4)
-    await insert_selection("user1", 2, 2, 3)
+    await db.insert_selection("user1", 1, 2, 4)
+    await db.insert_selection("user1", 2, 2, 3)
+    n_ins = 5
 
     # count rows
     async with pool.connection() as conn:
         async with conn.cursor() as cur:
             await cur.execute("select count(1) as cnt from image_selections")
             res = await cur.fetchone()
-            assert int(res[0]) == 5
+            assert int(res[0]) == n_ins
 
     # 66% 2-3
-    result = await get_prop_selected(1, 2, 3)
+    result = await db.get_prop_selected(1, 2, 3)
     assert result[0] == 2
     assert result[1] == 3
 
     # 33% 3-2 (complement to the previous result)
-    result = await get_prop_selected(1, 3, 2)
+    result = await db.get_prop_selected(1, 3, 2)
     assert result[0] == 1
     assert result[1] == 3
 
     # no such id, zero all
-    result = await get_prop_selected(999, 2, 3)
+    result = await db.get_prop_selected(999, 2, 3)
     assert result[0] == 0
     assert result[1] == 0
 
     # error on duplicate insertion
     with pytest.raises(psycopg.errors.UniqueViolation):
-        await insert_selection("user1", 1, 2, 3)
+        await db.insert_selection("user1", 1, 2, 3)
+
+    new_ins = 100
+
+    async def concurrent_inserts():
+        tasks = [db.insert_selection(f"user{i}", 4, 1, 2) for i in range(new_ins)]
+        await asyncio.gather(*tasks)
+
+    await concurrent_inserts()
+
+    # count rows
+    async with pool.connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("select count(1) as cnt from image_selections")
+            res = await cur.fetchone()
+            assert int(res[0]) == n_ins + new_ins
+
+    # simulate long concurrent queries to make sure it is really async
 
 
-# @pytest.mark.asyncio
-# async def test_insert_selection2(setup_database):
-#     pool = setup_database
-#     await insert_selection("user1", 1, 2, 3)
-#     async with pool.connection() as conn:
-#         async with conn.cursor() as cur:
-#             await cur.execute("select count(1) as cnt from image_selections")
-#             res = await cur.fetchone()
-#             print(res)
-#             assert int(res[0]) == 1
+@pytest.mark.asyncio
+async def test_insert_selection2(setup_database):
+    pool = setup_database
+    await db.insert_selection("user1", 1, 2, 3)
+    async with pool.connection() as conn:
+        async with conn.cursor() as cur:
+            await cur.execute("select count(1) as cnt from image_selections")
+            res = await cur.fetchone()
+            print(res)
+            assert int(res[0]) == 1
 
 
 # @pytest.mark.asyncio
